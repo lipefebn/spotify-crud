9fb03b0bb4cceec987f9c5070e3273cf96134edb
lipefebn <felipe.nunes@cloudwalk.io>
2022-10-11 15:19:00 -0300

    fix path

--- docs/wiki/puml_to_png.sh
+++ docs/wiki/puml_to_png.sh
@@ -17,7 +17,7 @@ fi
 
 
 wget -q -O plantuml.jar https://github.com/plantuml/plantuml/releases/download/v1.2022.8/plantuml-1.2022.8.jar
-java -jar plantuml.jar -charset UTF-8 -output ./$OUTPUT "./$PATH_PUML/**.puml"
+java -jar plantuml.jar -charset UTF-8 -output "./$PATH_OUTPUT" "./$PATH_PUML/**.puml"
 ls ./$PATH_PUML
 echo ----------------------------
 ls ./$OUTPUT
