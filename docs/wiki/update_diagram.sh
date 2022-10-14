1b98f115b9f8c5a95aac85f3b24f131ccc332fb5
lipefebn <felipe.nunes@cloudwalk.io>
2022-10-13 23:05:18 -0300

    test

--- docs/wiki/update_diagram.sh
+++ docs/wiki/update_diagram.sh
@@ -1,15 +1,18 @@
 #!/bin/bash 
 
-# if [ -z "$TOKEN" ]; then
-#     echo "TOKEN is not specified"
-#     exit 1
-# fi
-if [ -z "$PATH_DIAGRAMS" ] || [[ "$PATH_DIAGRAMS" =~ ^[\.\/] ]]; then
-    echo "PATH_DIAGRAMS is not specified" #TODO
+if [ -z "$TOKEN" ]; then
+    echo "TOKEN is not specified"
     exit 1
 fi
+if [ -z "$PAGE_NAME" ]; then 
+    echo "PAGE_NAME is no specified"
+fi
+if [ -z "$PATH_DIAGRAMS" ] || [[ "$PATH_DIAGRAMS" =~ ^[\.\/] ]]; then
+    echo "PATH_DIAGRAMS must follow the established pattern"
+    exit 1 
+fi
 if [ -z "$OUTPUT_DIAGRAMS" ] || [[ "$OUTPUT_DIAGRAMS" =~ ^[\.\/] ]]; then
-    echo "OUTPUT_DIAGRAMS is no specified"
+    echo "OUTPUT_DIAGRAMS must follow the established pattern"
     exit 1
 fi
 
@@ -27,6 +30,7 @@ TEMP_REPO_NAME="wiki-repo"
 
 MESSAGE_COMMIT=`git log -1 --format="%s"`
 
+
 # Stop the execution
 function hasError() {
     if [ $? -gt 0 ]; then
@@ -41,7 +45,9 @@ function SetConfigsGit() {
     echo $email
     # set configs git
     git config --global user.email "$email"
+    hasError "Could not config git"
     git config --global user.name "$GITHUB_ACTOR"
+    hasError "Could not config git"
 }
 
 # Clone the wiki repository and change working directory to wiki repository
@@ -54,9 +60,6 @@ function getWikiRepository() {
     # move to wiki repository
     cd "$TEMP_REPO_NAME"
     hasError "Could not move to wiki repository"
-    
-    # remove old file
-    rm Diagrams.md
 }
 
 # Function to generate the png files
@@ -75,31 +78,34 @@ function pumlToPng() {
 
 # for each in png files and put in markdown
 function putEachPngFile() {
+    # remove old file
+    rm "$PAGE_NAME"
+
+    # get png files that are using snake_case 
     local files_png
     files_png=$(ls "$ROOT_OUTPUT_DIAGRAMS" -t -U | grep -oP "^[a-z]+(_[a-z]+)*\.png$")
     hasError "Could not get png files"
     
-    for i in $files_png; do
-        doMarkdown $i
+    for file in $files_png; do
+        doMarkdown $file
     done
 }
-
-# build markdown
+# build markdown for a file
 function doMarkdown() {
     local file="[[$OUTPUT_DIAGRAMS/$1|alt=$1]]"
-    local title=$(getNameToTitle $1)
+    local title=$(getTitle $1)
 
-    echo "## $title" >> Diagrams.md
-    echo "$file" >> Diagrams.md # image
+    echo "## $title" >> "$PAGE_NAME"
+    echo "$file" >> "$PAGE_NAME" # image
 }
-
-# build the header for each diagram
-function getNameToTitle() {
+# build the title for each diagram
+function getTitle() {
     local title=`expr match "$1" '\([a-z_]*\)'` # remove .png
     title=${title//_/ } # replace "_" to blank space
     title=${title^} # first letter to uppercase
     echo $title
 }
+
 
 function doPush() {
     if [ -z "$(git status --porcelain)" ]; then 
@@ -115,13 +121,13 @@ function doPush() {
 }
 
 
+echo "configuring git..."
+SetConfigsGit
 echo "cloning the wiki repository..."
 getWikiRepository
 echo "generating png files..."
 pumlToPng
 echo "generating the markdown file..."
 putEachPngFile
-echo "configuring git..."
-SetConfigsGit
 echo "starting the function doPush..."
 doPush
