2705daec33b5f2c8302b1a1a40423dbdafe0f765
lipefebn <felipe.nunes@cloudwalk.io>
2022-10-15 12:05:49 -0300

    fix regex

--- docs/wiki/test.sh
+++ docs/wiki/test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash 
 
 ROOT_PATH_PUML="/home/felipenunes-e14/Documents/spotify/spotify-crud/docs/diagram"
-test="diagrama_de_test.png"
+test="primeiro.png"
 
 function getTitle() {
-    local file_name_puml=`expr match "$1" '\([a-z_]*\)'` # remove .png
+    local file_name_puml=${1//.png/} # remove .png
+    echo $file_name_puml
     file_name_puml+=".puml"
     local root_file_name_puml="$ROOT_PATH_PUML/$file_name_puml"
-    local title=$(cat $root_file_name_puml | grep -Po "(?<=title ).+")
+    local title=$(cat $root_file_name_puml | grep -Po "(?<=^title ).+$")
     echo $title
 }
 
--- docs/wiki/update_diagram.sh
+++ docs/wiki/update_diagram.sh
@@ -8,33 +8,18 @@ if [ -z "$PAGE_NAME" ]; then
     echo "PAGE_NAME is no specified"
     exit 1
 fi
-if [ -z "$PATH_DIAGRAMS" ] || [[ "$PATH_DIAGRAMS" =~ ^[\.\/] ]]; then
-    echo "PATH_DIAGRAMS must follow the established pattern"
+if [ -z "$PATH_PUML" ] || [[ "$PATH_PUML" =~ ^[\.\/] ]]; then
+    echo "PATH_PUML must follow the established pattern"
     exit 1 
 fi
-if [ -z "$OUTPUT_DIAGRAMS" ] || [[ "$OUTPUT_DIAGRAMS" =~ ^[\.\/] ]]; then
-    echo "OUTPUT_DIAGRAMS must follow the established pattern"
+if [ -z "$OUTPUT" ] || [[ "$OUTPUT" =~ ^[\.\/] ]]; then
+    echo "OUTPUT must follow the established pattern"
     exit 1
 fi
-
+# set the default message if no message is specified.
 if [ -z "$MESSAGE_COMMIT" ]; then
     MESSAGE_COMMIT="Update wiki"
 fi
-
-cd .. # return to root path
-
-# Put '/'
-OUTPUT_DIAGRAMS="/$OUTPUT_DIAGRAMS"
-PATH_DIAGRAMS="/$PATH_DIAGRAMS"
-
-# get absolute path
-ROOT_PATH_PUML="${GITHUB_WORKSPACE}${PATH_DIAGRAMS}"
-echo $ROOT_PATH_PUML
-
-# The default name for the wiki repository.
-TEMP_REPO_NAME="wiki-repo" 
-
-
 
 # Stop the execution
 function hasError() {
@@ -44,29 +29,28 @@ function hasError() {
     fi
 }
 
-function SetConfigsGit() {
-    # get configs git
-    local email=`git log -1 --format="%ae"`
-    echo $email
-    # set configs git
-    git config --global user.email "$email"
-    hasError "Could not config git"
-    git config --global user.name "$GITHUB_ACTOR"
-    hasError "Could not config git"
-}
+cd .. # return to root path
+
+# Put '/' in paths
+OUTPUT="/$OUTPUT"
+PATH_PUML="/$PATH_PUML"
+
+# The default name for the wiki repository.
+TEMP_REPO_NAME="wiki-repo" 
+
+# get absolute path
+ROOT_OUTPUT="$(pwd)/${TEMP_REPO_NAME}${OUTPUT}"
+ROOT_PATH_PUML="${GITHUB_WORKSPACE}${PATH_PUML}"
+
 
 # Clone the wiki repository and change working directory to wiki repository
 function getWikiRepository() {
-    hasError "Could not return to root path"
-
     git clone "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "$TEMP_REPO_NAME"
     hasError "Could not clone repo"
     
     # move to wiki repository
     cd "$TEMP_REPO_NAME"
     hasError "Could not move to wiki repository"
-
-    ROOT_OUTPUT_DIAGRAMS="$(pwd)"$OUTPUT_DIAGRAMS #Get root path to OUTPUT
 }
 
 # Function to generate the png files
@@ -77,7 +61,7 @@ function pumlToPng() {
     wget -q -O $file_jar https://github.com/plantuml/plantuml/releases/download/v1.2022.8/plantuml-1.2022.8.jar
     hasError "Could not get plantuml.jar"
 
-    java -jar $file_jar -charset UTF-8 -output $ROOT_OUTPUT_DIAGRAMS "$ROOT_PATH_PUML/**.puml"
+    java -jar $file_jar -charset UTF-8 -output $ROOT_OUTPUT "$ROOT_PATH_PUML/**.puml"
     hasError "Could not generate png files"
     
     rm $file_jar
@@ -85,12 +69,12 @@ function pumlToPng() {
 
 # for each in png files and put in markdown
 function putEachPngFile() {
-    # remove old file
+    # remove old markdown file
     rm "$PAGE_NAME"
 
-    # get png files that are using snake_case 
+    # get all png files 
     local files_png
-    files_png=$(ls "$ROOT_OUTPUT_DIAGRAMS" -t -U | grep -oP "^[a-z]+(_[a-z]+)*\.png$")
+    files_png=$(ls "$ROOT_OUTPUT" -t -U | grep -oP "^.+\.png$")
     hasError "Could not get png files"
     
     for file in $files_png; do
@@ -99,21 +83,33 @@ function putEachPngFile() {
 }
 # build markdown for a file
 function doMarkdown() {
-    local file="[[$OUTPUT_DIAGRAMS/$1|alt=$1]]"
+    local file="[[$OUTPUT/$1|alt=$1]]"
     local title=$(getTitle $1)
 
     echo "## $title" >> "$PAGE_NAME"
     echo "$file" >> "$PAGE_NAME" # image
 }
-# build the title for each diagram
+# get the tittle in puml file
 function getTitle() {
-    local file_name_puml=`expr match "$1" '\([a-z_]*\)'` # remove .png
+    local file_name_puml=${1//.png/} # remove .png
     file_name_puml+=".puml"
+    
     local root_file_name_puml="$ROOT_PATH_PUML/$file_name_puml"
-    local title=$(cat $root_file_name_puml | grep -Po "(?<=title ).+")
+    local title=$(cat $root_file_name_puml | grep -Po "(?<=^title ).+$")
     echo $title
 }
 
+function SetConfigsGit() {
+    # get configs git
+    local email=`git log -1 --format="%ae"`
+    
+    # set configs git
+    git config --global user.email "$email"
+    hasError "Could not config git"
+
+    git config --global user.name "$GITHUB_ACTOR"
+    hasError "Could not config git"
+}
 
 function doPush() {
     if [ -z "$(git status --porcelain)" ]; then 
@@ -131,11 +127,15 @@ function doPush() {
 
 echo "cloning the wiki repository..."
 getWikiRepository
-echo "configuring git..."
-SetConfigsGit
+
 echo "generating png files..."
 pumlToPng
+
 echo "generating the markdown file..."
 putEachPngFile
+
+echo "configuring git..."
+SetConfigsGit
+
 echo "starting the function doPush..."
 doPush
