81b9fa6721a4392e7ce6dd8b7e2da55fd56d6166
lipefebn <felipe.nunes@cloudwalk.io>
2022-10-17 18:18:28 -0300

    test

--- docs/wiki/test.sh
+++ docs/wiki/test.sh
@@ -1,15 +1,14 @@
-#!/bin/bash 
+#!/bin/bash
+# sirene.sh
+# Bom para chamar a atenção dos colegas de trabalho :)
 
-ROOT_PATH_PUML="/home/felipenunes-e14/Documents/spotify/spotify-crud/docs/diagram"
-test="primeiro.png"
-
-function getTitle() {
-    local file_name_puml=${1//.png/} # remove .png
-    echo $file_name_puml
-    file_name_puml+=".puml"
-    local root_file_name_puml="$ROOT_PATH_PUML/$file_name_puml"
-    local title=$(cat $root_file_name_puml | grep -Po "(?<=^title ).+$")
-    echo $title
-}
-
-getTitle $test
+echo -ne "\033[11;2000]"   # Cada bipe dura quase um segundo
+while :                   # Loop eterno
+do
+	echo -ne "\033[100;100]\a" ; sleep 0.5  # Tom alto (agudo)
+	echo -ne "\033[10;400]\a" ; sleep 0.5  # Tom baixo (grave)
+	echo -ne "\033[10;400]\a" ; sleep 0.5  # Tom baixo (grave)
+	echo -ne "\033[10;400]\a" ; sleep 0.5  # Tom baixo (grave)
+	echo -ne "\033[10;400]\a" ; sleep 1  # Tom baixo (grave)
+	echo -ne "\033[10;400]\a" ; sleep 1  # Tom baixo (grave)
+done
