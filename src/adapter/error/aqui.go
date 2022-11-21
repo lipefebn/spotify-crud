9c3ca50d8592a99b0c1d5744b822267b90dac3f0
lipefebn <felipe.nunes@cloudwalk.io>
2022-11-21 15:22:40 -0300

    test

--- src/adapter/error/aqui.go
+++ src/adapter/error/aqui.go
@@ -3,6 +3,6 @@ package error
 
 var (
 	TEST                       = NewRest("SLA_CLIENT_123", "SLA123", SLA, 700)
-	AFF                       = NewRest("AGG_CLIENT_123", "AGG123", AGG, 700)
+	AFG                        = NewRest("AGG_CLIENT_123", "AGG123", AGG, 700)
 
 )
